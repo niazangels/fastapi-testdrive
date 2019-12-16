f6bf5eeebc8e61c36e78d6407cfb230f9fee9007
Niyas Mohammed <niaz.angels@gmail.com>
2019-12-16 23:34:07 +0530

    Add /user/me, /user/{user_id}

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ async def root():
     return {"message": "Sup, bro?"}
 
 
-@app.get("/item/{item_id}")
+@app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+# Because path operations are evaluated in order, you need to make sure
+# that the path for /users/me is declared before the one
+# for /users/{user_id}:app.get("/user/me")
+@app.get("/user/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/user/{user_id}")
+async def read_user(user_id: int):
+    return {"user_id": user_id}
