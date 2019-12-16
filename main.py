de2ba9c5fc5cd62084778421cf6e1e46ac6b9cc1
Niyas Mohammed <niaz.angels@gmail.com>
2019-12-16 23:23:43 +0530

    Add /item/{item_id} endpoint

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Sup, bro?"}
+
+
+@app.get("/item/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
